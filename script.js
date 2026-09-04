eabfb3ffcd5a46294b259e72d2427dcb634b64fa
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-04 11:08:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table=document.getElementById('sampleTable');
+	let row=document.createElement('tr');
+	let cell1=document.createElement('td');
+	cell1.textContent="New Cell1";
+	let cell2=document.createElement('td');
+	cell2.textContent="New Cell2";
+	row.appendChild(cell1);
+	row.appendChild(cell2);
+	table.prepend(row);
 }
